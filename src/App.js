96a8f38ab2dbc03a75e7e9e4855fff63ba4ca9a5
Kjohnson870 <karjohnson08@yahoo.com>
2023-07-30 13:43:42 -0500

    fakeshop

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import React from "react";
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
